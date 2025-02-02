3e80d651bf1db3abd82f70ecb926a573c32fbbc1
Кузичев Александр <pulsarmn@yandex.ru>
2025-02-02 14:43:47 +0300

    add: create the TicketRepository interface

---
+++ backend/src/main/java/com/dorm/repository/TicketRepository.java
@@ -0,0 +1,12 @@
+package com.dorm.repository;
+
+import com.dorm.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+
+    List<Ticket> findByCreationDate(LocalDateTime creationDate);
+}
