4d85a2cd5c9d440d22cbe184134c520f4e76bc52
Кузичев Александр <pulsarmn@yandex.ru>
2025-02-02 15:31:35 +0300

    add: implement the findAll() method in the TicketService class

--- backend/src/main/java/com/dorm/service/TicketService.java
+++ backend/src/main/java/com/dorm/service/TicketService.java
@@ -1,9 +1,13 @@
 package com.dorm.service;
 
+import com.dorm.dto.ReadTicketDto;
 import com.dorm.mapper.ReadTicketDtoMapper;
 import com.dorm.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -11,4 +15,10 @@ public class TicketService {
 
     private final TicketRepository ticketRepository;
     private final ReadTicketDtoMapper readTicketDtoMapper;
+
+    public List<ReadTicketDto> findAll() {
+        return ticketRepository.findAll().stream()
+                .map(readTicketDtoMapper::mapFrom)
+                .collect(Collectors.toList());
+    }
 }
