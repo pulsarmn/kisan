e2e03d4f99bca1982ca80f5f6992b518a455aba6
Кузичев Александр <pulsarmn@yandex.ru>
2025-02-02 14:06:39 +0300

    add: create the User entity class

---
+++ backend/src/main/java/com/dorm/entity/User.java
@@ -0,0 +1,27 @@
+package com.dorm.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
