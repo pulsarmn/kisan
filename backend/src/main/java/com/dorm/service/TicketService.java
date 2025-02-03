43c7cd278135862dc93928a4b2ed2fc9e7110c3b
Кузичев Александр <pulsarmn@yandex.ru>
2025-02-03 15:06:12 +0300

    add: implement the findById() method in the TicketService class

--- backend/src/main/java/com/dorm/service/TicketService.java
+++ backend/src/main/java/com/dorm/service/TicketService.java
@@ -2,23 +2,34 @@ package com.dorm.service;
 
 import com.dorm.dto.ReadTicketDto;
 import com.dorm.mapper.ReadTicketDtoMapper;
+import com.dorm.mapper.TicketMapper;
 import com.dorm.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
+
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class TicketService {
 
     private final TicketRepository ticketRepository;
     private final ReadTicketDtoMapper readTicketDtoMapper;
+    private final TicketMapper ticketMapper;
 
     public List<ReadTicketDto> findAll() {
         return ticketRepository.findAll().stream()
                 .map(readTicketDtoMapper::mapFrom)
                 .collect(Collectors.toList());
+    }
+
+    public Optional<ReadTicketDto> findById(Integer id) {
+        return ticketRepository.findById(id)
+                .map(readTicketDtoMapper::mapFrom);
     }
 }
