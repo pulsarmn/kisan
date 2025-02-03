613275f1f96cc1a5e378790972cfe7a7b1cc2eb7
Кузичев Александр <pulsarmn@yandex.ru>
2025-02-03 15:54:10 +0300

    add: implement the getTicket() method in the TicketRestController class

--- backend/src/main/java/com/dorm/http/rest/TicketRestController.java
+++ backend/src/main/java/com/dorm/http/rest/TicketRestController.java
@@ -1,7 +1,12 @@
 package com.dorm.http.rest;
 
+import com.dorm.dto.ReadTicketDto;
 import com.dorm.service.TicketService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TicketRestController {
 
     private final TicketService ticketService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReadTicketDto> getTicket(@PathVariable("id") Integer id) {
+        return ticketService.findById(id)
+                .map(ticket -> ResponseEntity
+                        .ok()
+                        .body(ticket))
+                .orElse(ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .build());
+    }
 }
